9d40fce10b3e06a4f2467366377a2bc3d3ea4cea
Ellyboi <vitoesitemitope@gmail.com>
2023-05-05 14:41:20 +0100

    Bookstore setup and installations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './routes/Home';
+import Categories from './routes/Categories';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="booknav-container">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
